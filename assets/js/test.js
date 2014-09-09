4cbf8f717b3b9195cfd0cb124d69ca8b3d6a9036
lanshayu <fingnet@gmail.com>
2014-09-10 00:00:45 +0800

    IE7推送测试

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,14 +1,15 @@
-var socket = io.connect('http://localhost:1337');
+var socket = io.connect('http://192.168.31.230:1337');
 var user_id = location.search.slice(1) || '123';
 socket.on('connect',function(){
   socket.get('/notice',{user_id:user_id},function(users){
-    console.log(users);
+    //console && console.log(users);
   });
   socket.on('notice',function(e){
-  	console.log('Message:',e)
+  	//console && console.log('Message:',e)
+  	alert(e.data.content);
   });
 });
 
 socket.on('disconnect', function(e){
-	console.log('disconnect');
+	//console && console.log('disconnect');
 });
